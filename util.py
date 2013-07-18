2d7381d98fa3147baae7788ee3a2a5932d3c2822
Adam Eggum <aeggum12@gmail.com>
2013-07-17 23:10:09 -0500

    Java files removed along with binary files during clean up

--- util.py
+++ util.py
@@ -8,13 +8,16 @@ import sys
 class Util(object):
     
     @staticmethod
-    def clean_up():
+    def clean_up(filenames):
         '''
         Removes any compiled files in the current directory.
         '''
         filelist = [ f for f in os.listdir(".") if (f.endswith(".class") or f.endswith(".pyc"))  ]
         for f in filelist:
             os.remove(f)
+        
+        for filename in filenames:
+            os.remove(filename)
     
     @staticmethod
     def compile_java(java_files):
